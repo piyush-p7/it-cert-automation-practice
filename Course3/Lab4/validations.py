65608b107f6c467ef151daeebe407f43f8cd520e
Piyush <piyushpandita151@gmail.com>
2022-08-24 11:21:59 +0530

    Corrected validations.py of Git and GitHub by Google.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import re
 
 def validate_user(username, minlen):
@@ -9,13 +8,13 @@ def validate_user(username, minlen):
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
-    # Usernames can't be shorter than minlen
+    # Usernames can't be shorter than minlen.
     if len(username) < minlen:
         return "Username must not be less than {} character.".format(minlen)
-    # Usernames can only use letters, numbers, dots and underscores
+    # Usernames can only use letters, numbers, dots and underscores.
     if not re.match('^[a-z0-9._]*$', username):
         return False
-    # Usernames can't begin with a symbol
+    # Usernames can't begin with a symbol.
     if username[0].isalpha():
         return False
     return True
